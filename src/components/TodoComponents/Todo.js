fc83807845008c1931cad8fa931ca7bf1993a6d3
Braden <fish2355@hotmail.com>
2020-01-09 14:59:06 -0700

    commit

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,3 +1,24 @@
+// import React from "react";
+
+// function Todo({ todo, remove, complete, notComplete }) {
+//   return (
+//     <div className="todo">
+//       <h3 className={todo.completed ? "completed" : "todo-task"}>
+//         {todo.task}
+//       </h3>
+//       <div className="btn-div">
+//         <button className="btn-complete" id={todo.id} onClick={complete}>
+//           Complete
+//         </button>
+//         <button className="btn-remove" id={todo.id} onClick={remove}>
+//           Remove
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Todo;
+
 import React from "react";
 
 function Todo({ todo, remove, complete, notComplete }) {
